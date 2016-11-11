87f97ac130fd180086273e5ac7de71e9826bcb0d
Alessandro Elias <ae11@c3sl.ufpr.br>
2016-11-11 10:49:24 -0200

    Fix decrementa_chave; but busca_elem expect to be ordered, decrement_key needs de pos of vertex, how to manage this index?
    Signed-off-by: Alessandro Elias <ae11@c3sl.ufpr.br>

--- grafo.c
+++ grafo.c
@@ -272,14 +272,14 @@ void print_a(vertice, lista);
 void print_v(grafo);
 void print_vattr(grafo);
 void print_vbylista(lista);
-void print_heap(heap*, unsigned int);
+void print_heap(heap*);
 #else
 
 #define print_a(vertice, lista)		(void)0
 #define print_v(grafo)				(void)0
 #define print_vattr(grafo)			(void)0
 #define print_vbylista(lista)		(void)0
-#define print_heap(heap, size)		(void)0
+#define print_heap(heap)			(void)0
 
 #endif /* DEBUG */
 
@@ -677,8 +677,8 @@ grafo le_grafo(FILE *input) {
     // binary heap
     //http://www.cs.princeton.edu/~wayne/cs423/lectures/heaps-4up.pdf
     vertice u, v;
-    u = busca_vertice("a", g->vertices);
-    v = busca_vertice("b", g->vertices);
+    u = busca_vertice("A", g->vertices);
+    v = busca_vertice("F", g->vertices);
     caminho_minimo(u, v, g);
 
     return g;
@@ -804,20 +804,23 @@ void troca(heap *h, uint i, uint i2) {
 	h[i2] = utmp;
 }
 
-void decrementa_chave(heap *h, vertice u, uint pos) {
-	uint i, ipai = 0;
+void decrementa_chave(heap *h, uint pos) {
+	uint pai_ant, pai = 0;
 
-	print_heap(h, pos);
-	ipai = pos;
-	i = pos - 1;
-	while( (ipai = pai(ipai)) > 0 ) {
-		if( h[ipai]->distancia > u->distancia ) {
-			troca(h, i, ipai);
-			i = ipai;
-			print_heap(h, pos);
+	print_heap(h);
+	pai_ant = pai = pos;
+	while( (pai = pai(pai)) > 0 ) {
+		if( h[pai]->distancia > h[pai_ant]->distancia ) {
+			troca(h, pai_ant, pai);
+			pai_ant = pai;
+			print_heap(h);
 		}
 	}
 }
+
+//------------------------------------------------------------------------------
+// Faz o bubble down; para pegar o i-esimo elemento e descer na min heap ate
+// estar ordenado.
 
 void bolha_baixo(heap *h, uint i) {
 	uint pai, fesq, fdir;
@@ -834,7 +837,7 @@ void bolha_baixo(heap *h, uint i) {
 			troca(h, pai, fdir);
 			pai = fdir;
 		}
-		print_heap(h, heap_pos);
+		print_heap(h);
 		fesq = filho_esq(pai);
 		fdir = filho_dir(pai);
 	}
@@ -844,11 +847,38 @@ vertice remove_min(heap *h) {
 	vertice v = (vertice)h[1];
 	troca(h, --heap_pos, 1);
 	h[heap_pos] = NULL;
-	print_heap(h, heap_pos);
+	print_heap(h);
 	bolha_baixo(h, 1);
-	bolha_baixo(h, 1);		// verifique se subiu um menor que o lado direito.
-
+	bolha_baixo(h, 1);		// verifique se subiu um menor que o lado direito
+							// e este pode estar na raiz.
 	return v;
+}
+
+uint busca_elem_heap(heap *h, heap ele) {
+	uint pai, fesq, fdir, pos;
+
+	if( h[1] == ele )
+		return 1;
+
+	pai = 1;
+	fesq = filho_esq(pai);
+	fdir = filho_dir(pai);
+	pos = 0;
+	while( !pos && (fesq < heap_pos || fdir < heap_pos) ) {
+		if( fesq < heap_pos && h[fesq] == ele )
+			pos = fesq;
+		else if( fdir < heap_pos && h[fdir] == ele )
+			pos = fdir;
+		else {
+			if( ele->distancia > h[fesq]->distancia )
+				pai = fesq;
+			else pai = fdir;
+		}
+		fesq = filho_esq(pai);
+		fdir = filho_dir(pai);
+	}
+
+	return pos;
 }
 
 heap* constroi_heap(grafo g) {
@@ -856,16 +886,16 @@ heap* constroi_heap(grafo g) {
 	vertice u;
 	heap*	h;
 
-	h = (heap*)malloc((g->narestas+1) * sizeof(heap));
-	memset(h, 0, (g->narestas+1) * sizeof(heap));
+	h = (heap*)malloc((g->narestas+2) * sizeof(heap));
+	memset(h, 0, (g->narestas+2) * sizeof(heap));
 
 	n = primeiro_no(g->vertices);
 	h[1] = conteudo(n);
 	heap_size = 2;
 	for( n=proximo_no(n); n; n=proximo_no(n) ) {
 		u = conteudo(n);
-		h[heap_size++] = u;
-		decrementa_chave(h, u, heap_size);
+		h[heap_size] = u;
+		decrementa_chave(h, heap_size++);
 	}
 	heap_pos = heap_size;
 
@@ -885,13 +915,13 @@ lista caminho_minimo(vertice u, vertice v, grafo g) {
 	lint dist;
 	heap* h;
 
-	srandom(time(NULL));
+//	srandom(time(NULL));
 	lista T = NULL;
 	for( n=primeiro_no(g->vertices); n; n=proximo_no(n) ) {
 		ucorr = conteudo(n);
 		ucorr->estado = NaoVisitado;
-//		ucorr->distancia = infinito;
-		ucorr->distancia = rand() % 100;
+		ucorr->distancia = infinito;
+//		ucorr->distancia = rand() % 100;
 		ucorr->anterior = NULL;
 	}
 
@@ -899,26 +929,22 @@ lista caminho_minimo(vertice u, vertice v, grafo g) {
 	u->distancia = 0;
 	print_vattr(g);
 	h = constroi_heap(g);
-	for( int i=1; i < heap_size; i++ ) {
+	while( *(h+1) != NULL ) {
 		ucorr = remove_min(h);
+		ucorr->estado = Visitado;
+
+		for( n=primeiro_no(ucorr->vizinhos_esq); n; n=proximo_no(n) ) {
+			a = conteudo(n);
+			if( a->destino->estado == NaoVisitado ) {
+				dist = a->origem->distancia + a->peso;
+				if( dist < a->destino->distancia ) {
+					a->destino->distancia = dist;
+					a->destino->anterior = ucorr;
+					decrementa_chave(h, busca_elem_heap(h, a->destino));
+				}
+			}
+		}
 	}
-	free(h);
-//	while( heap_size > 0 ) {
-//		ucorr = conteudo(n);
-//		ucorr->estado = Visitado;
-//		remove_no(Q, n, NULL);
-//
-//		for( n=primeiro_no(ucorr->vizinhos_esq); n; n=proximo_no(n) ) {
-//			a = conteudo(n);
-//			if( a->destino->estado == NaoVisitado ) {
-//				dist = a->origem->distancia + a->peso;
-//				if( dist < a->destino->distancia ) {
-//					a->destino->distancia = dist;
-//					a->destino->anterior = ucorr;
-//				}
-//			}
-//		}
-//	}
 
 	return T;
 }
