877ac92d5f1805469eb3331e12af764f0c98c8b8
Alessandro Elias <ae11@c3sl.ufpr.br>
2016-11-20 10:45:56 -0200

    Merge with tag deliver-rc-0.1 and add debug to le_grafo.
    Signed-off-by: Alessandro Elias <ae11@c3sl.ufpr.br>

--- grafo.c
+++ grafo.c
@@ -678,6 +678,54 @@ grafo le_grafo(FILE *input) {
     constroi_grafo(Ag_g, g);
     agclose(Ag_g);
 
+    //https://www.youtube.com/watch?v=gdmfOwyQlcI
+    // binary heap
+    //http://www.cs.princeton.edu/~wayne/cs423/lectures/heaps-4up.pdf
+//    vertice u, v;
+//    u = busca_vertice("A", g->vertices);
+//    v = busca_vertice("F", g->vertices);
+//    lista T = caminho_minimo(u, v, g);
+//    print_vbylista(T);
+//    destroi_lista(T, NULL);
+
+//    lista **T2 = (lista**)calloc(g->nvertices, sizeof(lista**));
+//      lista **p = T2;
+//      for( no n=primeiro_no(g->vertices); n; n=proximo_no(n) )
+//              *p++ = (lista*)calloc(g->nvertices, sizeof(lista*));
+
+//      caminhos_minimos(T2, g);
+//      print_mat(T2, g);
+
+//    fprintf(stderr, "%d\n", direcionado(g));
+//    return g;
+    uint i;
+//      p = T2;
+//      for( i=0; i < g->nvertices; i++  ) {
+//              for( j=0; j< g->nvertices; j++ ) {
+//                      destroi_lista(T2[i][j], destroi_vertice);
+//              }
+//
+//              free(*p++);
+//      }
+//      free(T2);
+
+//      vertice u = busca_vertice("A", g->vertices);
+//      vertice v = busca_vertice("G", g->vertices);
+//      fprintf(stderr, "%ld\n", distancia(u, v, g));
+//      return g;
+//      fprintf(stderr, "%ld\n", diametro(g));
+
+        lint **dist = (lint**)calloc(g->nvertices, sizeof(lint**));
+        for( i=0; i < g->nvertices; i++ ) {
+                dist[i] = (lint*)calloc(g->nvertices, sizeof(lint*));
+        }
+        distancias(dist, g);
+        print_mat_dist(dist, g);
+        for( i=0; i < g->nvertices; i++ ) {
+                free(dist[i]);
+        }
+        free(dist);
+
     return g;
 }
 
