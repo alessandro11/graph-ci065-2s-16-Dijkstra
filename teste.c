a47bd395f9eee93dc6bd84fea6402eee053b6374
Alessandro Elias <ae11@c3sl.ufpr.br>
2016-11-20 21:56:52 -0200

    Custom the tests.
    Signed-off-by: Alessandro Elias <ae11@c3sl.ufpr.br>

--- teste.c
+++ teste.c
@@ -5,6 +5,7 @@
 typedef unsigned int uint;
 typedef long int lint;
 typedef struct no *no;
+typedef struct vertice* heap;
 
 typedef enum __Estado {
 	NaoVisitado = 0,
@@ -32,7 +33,31 @@ struct grafo {
 
 
 int destroi_vertice(void *v);
-void print_mat(lista **m, grafo g);
+vertice busca_vertice(const char*, lista);
+
+#ifdef DEBUG
+
+#define UNUSED(x)				(void)(x)
+
+void print_a(vertice, lista);
+void print_v(grafo);
+void print_vattr(grafo);
+void print_vbylista(lista);
+void print_heap(heap*);
+void print_mat(lista**, grafo);
+void print_mat_dist(lint**, grafo);
+
+#else
+
+#define print_a(vertice, lista)		(void)0
+#define print_v(grafo)				(void)0
+#define print_vattr(grafo)			(void)0
+#define print_vbylista(lista)		(void)0
+#define print_heap(heap)			(void)0
+#define print_mat(lista, grafo)		(void)0
+#define print_mat_dist(dist_m, grafo)(void)0
+
+#endif /* DEBUG */
 
 
 //------------------------------------------------------------------------------
@@ -42,27 +67,31 @@ int main(int argc, char* argv[]) {
   grafo g = le_grafo(fs);
   fclose(fs);
 
-  if ( !g )
-
-    return 1;
-
-
-  //https://www.youtube.com/watch?v=gdmfOwyQlcI
-  // binary heap
-  //http://www.cs.princeton.edu/~wayne/cs423/lectures/heaps-4up.pdf
-
-//	fprintf(stderr, "%d\n", direcionado(g));
-//	fprintf(stderr, "%ld\n", diametro(g));
+//  if( !g )
+//    return 1;
 
 
-//    vertice u, v;
-//    u = busca_vertice("A", g->vertices);
-//    v = busca_vertice("F", g->vertices);
-//    lista T = caminho_minimo(u, v, g);
-//    print_vbylista(T);
-//    destroi_lista(T, NULL);
+	//https://www.youtube.com/watch?v=gdmfOwyQlcI
+	//binary heap
+	//http://www.cs.princeton.edu/~wayne/cs423/lectures/heaps-4up.pdf
 
-	lista **T2 = (lista**)calloc(g->nvertices, sizeof(lista**));
+    vertice u, v;
+    lista T, **T2;
+
+    uint i, j;
+
+	fprintf(stderr, "%d\n", direcionado(g));
+	fprintf(stderr, "%ld\n", diametro(g));
+
+    u = busca_vertice("A", g->vertices);
+    v = busca_vertice("G", g->vertices);
+	fprintf(stderr, "%ld\n", distancia(u, v, g));
+
+    T = caminho_minimo(u, v, g);
+    print_vbylista(T);
+    destroi_lista(T, NULL);
+
+	T2 = (lista**)calloc(g->nvertices, sizeof(lista**));
 	lista **p = T2;
 	for( no n=primeiro_no(g->vertices); n; n=proximo_no(n) )
 		*p++ = (lista*)calloc(g->nvertices, sizeof(lista*));
@@ -70,7 +99,6 @@ int main(int argc, char* argv[]) {
 	caminhos_minimos(T2, g);
 	print_mat(T2, g);
 
-    uint i, j;
 	p = T2;
 	for( i=0; i < g->nvertices; i++  ) {
 		for( j=0; j< g->nvertices; j++ ) {
@@ -80,21 +108,18 @@ int main(int argc, char* argv[]) {
 	}
 	free(T2);
 
-//      vertice u = busca_vertice("A", g->vertices);
-//      vertice v = busca_vertice("G", g->vertices);
-//      fprintf(stderr, "%ld\n", distancia(u, v, g));
+	lint **dist = (lint**)calloc(g->nvertices, sizeof(lint**));
+	for( i=0; i < g->nvertices; i++ ) {
+		dist[i] = (lint*)calloc(g->nvertices, sizeof(lint*));
+	}
 
+	distancias(dist, g);
+	print_mat_dist(dist, g);
 
-//        lint **dist = (lint**)calloc(g->nvertices, sizeof(lint**));
-//        for( i=0; i < g->nvertices; i++ ) {
-//                dist[i] = (lint*)calloc(g->nvertices, sizeof(lint*));
-//        }
-//        distancias(dist, g);
-//        print_mat_dist(dist, g);
-//        for( i=0; i < g->nvertices; i++ ) {
-//                free(dist[i]);
-//        }
-//        free(dist);
+	for( i=0; i < g->nvertices; i++ ) {
+		free(dist[i]);
+	}
+	free(dist);
 
 
 
